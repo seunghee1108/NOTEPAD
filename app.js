f12b8eadca29aff39cca6b6b813004fe3505fb43
승희 <bangseunghee1108@gmail.com>
2023-11-14 17:35:12 +0900

    server branch check

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const port =  3000;
+const path = require('path');
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('test');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
