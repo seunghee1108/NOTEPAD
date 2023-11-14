f55e75311157127e82582cf660af565db1b8961e
승희 <bangseunghee1108@gmail.com>
2023-11-14 14:20:23 +0900

    selectors 변수 묶음 작성

---
+++ public/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const selectors = {
+    memoForm: document.getElementById('memo-form'),
+    title: document.getElementById('title'),
+    content: document.getElementById('content'),
+    saveBtn: document.getElementById('saveBtn'),
+    memoList: document.querySelector('.memo-list')
+  };
+
+});
