b92871c07b49c4eaeb3905367c07842b3d73ecf6
승희 <bangseunghee1108@gmail.com>
2023-11-16 06:39:18 +0900

    port 문제가 있었음, 해결 완료

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const jsonFolderPath = path.join(__dirname, 'data');
-const jsonFilePath = path.join(jsonFolderPath, 'data.json');
+
 const port = 3000;
 
-const memoRouter = require('./router/routes');
+const jsonFilePath = path.join(__dirname, "data.json");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+const memoRouter = require('./routes/routes');
 app.use('/api', memoRouter);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// 에러 핸들링 미들웨어
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
 });
 
 app.listen(port, () => {
