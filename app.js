0af9224f1de73cf7b88b748d5fcea67132ea2054
승희 <bangseunghee1108@gmail.com>
2023-11-15 23:45:32 +0900

    코드 수정중

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const jsonFolderPath = path.join(__dirname, 'data');
+const jsonFilePath = path.join(jsonFolderPath, 'data.json');
 const port = 3000;
+
+const memoRouter = require('./router/routes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api', memoRouter);
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// 에러 핸들링 미들웨어
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
 });
 
 app.listen(port, () => {
