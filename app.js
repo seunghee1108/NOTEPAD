2d952846177be08354c1818731edae3e895fdb46
승희 <bangseunghee1108@gmail.com>
2023-11-16 09:44:08 +0900

    server 모듈화

--- app.js
+++ app.js
@@ -1,37 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-const path = require('path');
+const memoRoutes = require('./routes/routes');
+
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 
 app.use(bodyParser.json());
-
 app.use(express.static('public'));
 
 
-app.post('/api/memos', (req, res) => {
-  try {
-    const dataPath = path.join(__dirname, 'data', 'data.json');
+app.use('/api', memoRoutes);
 
-   
-    const memos = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
 
-    const newMemo = req.body;
-    memos.push(newMemo);
-
-    
-    fs.writeFileSync(dataPath, JSON.stringify(memos, null, 2), 'utf-8');
-
-    res.status(201).json(newMemo); 
-  } catch (error) {
-    console.error('Error adding memo:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-app.listen(port, () => {
-  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+app.listen(PORT, () => {
+ console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
 });
